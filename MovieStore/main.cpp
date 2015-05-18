08b1dd6ac66f84ae79d0c74e3afc2ec2e2e30c27
Shen <shenl95@uw.edu>
2015-05-18 03:03:26 -0700

    main

--- MovieStore/main.cpp
+++ MovieStore/main.cpp
@@ -6,4 +6,24 @@
 //  Copyright (c) 2015年 Shen Li. All rights reserved.
 //
 
+#include <iostream>
+#include <fstream>
+#include "movieStore.h"
+using namespace std;
 
+
+int main() {
+    ifstream infile("");
+    if (!infile) {
+        cout << "File cannot be found" << endl;
+        return 1;
+    }
+    
+    MovieStore MOVIE;
+    
+    while (!infile.eof()) {
+        string processing;
+        getline(infile, processing);
+        MOVIE.process(processing);
+    }
+}
